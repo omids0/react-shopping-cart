8779cd70c08abda4679a7d809ed8aea9557e3f4f
Omid s0 <soheilniao@gmail.com>
2021-11-17 15:09:04 +0330

    add redux to cart

--- src/App.js
+++ src/App.js
@@ -6,38 +6,6 @@ import Products from './component/Products';
 import store from './store'
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem('cartItems')) : [],
-    }
-  }
-  createOrder = (order) => {
-    alert(order.name)
-  }
-  addToCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    let alredyInCart = false;
-    cartItems.forEach(item => {
-      if (item._id === product._id) {
-        item.count++;
-        alredyInCart = true;
-      }
-    });
-    if (!alredyInCart) {
-      cartItems.push({ ...product, count: 1 })
-    }
-    this.setState({ cartItems })
-    localStorage.setItem('cartItems', JSON.stringify(cartItems))
-  }
-  removeFromCart = (itemSelected) => {
-    const cartItems = this.state.cartItems.slice();
-    const removedItem = cartItems.filter(item => {
-      return item._id !== itemSelected._id
-    })
-    this.setState({ cartItems: removedItem })
-    localStorage.setItem('cartItems', JSON.stringify(removedItem))
-  }
   render() {
     return (
       <Provider store={store}>
@@ -49,16 +17,10 @@ class App extends React.Component {
             <div className='content'>
               <div className='main'>
                 <Filter />
-                <Products
-                  addToCart={this.addToCart}
-                />
+                <Products />
               </div>
               <div className='sidebar'>
-                <Cart
-                  cartItems={this.state.cartItems}
-                  removeFromCart={this.removeFromCart}
-                  createOrder={this.createOrder}
-                />
+                <Cart />
               </div>
             </div>
           </main>
